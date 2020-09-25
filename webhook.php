51806b416ad7befbc8d308df3a47d5719eaae64a
zrq <1053304669@qq.com>
2020-09-26 00:11:45 +0800

    update

--- webhook.php
+++ webhook.php
@@ -6,7 +6,7 @@ $secret = "hxs10.10";
 // Path to your respostory on your server.
 // e.g. "/var/www/respostory"
 // 项目地址
-$path = "/data/web/html/Detachement_Blog";
+$path = "/data/web/www/";
 
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
